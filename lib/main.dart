16eee15cefbe625a821f6f3b3c0de04bc19c7415
socurites <socurites@wise-fashion.com>
2021-03-11 13:03:17 +0900

    2021-03-11 studied

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_weather_in_action/models/src/app_settings.dart';
+import 'package:flutter_weather_in_action/styles.dart';
 
 void main() {
-  runApp(MyApp());
+  AppSettings settings = AppSettings();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]).then((_) => runApp(
+      MyApp(settings: settings),
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({Key key, this.settings}) : super(key: key);
+
+  final AppSettings settings;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    final theme = ThemeData(
+      fontFamily: "Cabin",
+      primaryColor: AppColor.midnightSky,
+      accentColor: AppColor.midnightCloud,
+      primaryTextTheme: Theme.of(context).textTheme.apply(
+        bodyColor: AppColor.textColorDark,
+        displayColor: AppColor.textColorDark,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      textTheme: Theme.of(context).textTheme.apply(
+        bodyColor: AppColor.textColorDark,
+        displayColor: AppColor.textColorDark,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: true,
+      theme: theme,
+      home: PageContainer(settings: settings),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class PageContainer extends StatefulWidget {
+  final AppSettings settings;
+
+  PageContainer({Key key, this.settings}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -43,13 +59,13 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String title = 'title';
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _PageContainerState createState() => _PageContainerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _PageContainerState extends State<PageContainer> {
   int _counter = 0;
 
   void _incrementCounter() {
